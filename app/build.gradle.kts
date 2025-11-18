082f51fc97b9b755e55fc2699f85e4ad2adae06b
mdasilvacerq <matt.scerqueira@gmail.com>
2025-11-18 13:38:54 -0300

    Corrigindo o gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.gs2androidapp"
-    compileSdk = 34 // Versão estável
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.gs2androidapp"
@@ -38,34 +38,20 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1" // Versão estável
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
-
 dependencies {
-    // Core KTX e Lifecycle
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-
-    // Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-
-    // Material 3 (TopAppBar, Scaffold, Button)
     implementation("androidx.compose.material3:material3")
-
-    // Ícones Básicos (Para o ArrowBack que resolve o erro)
     implementation("androidx.compose.material:material-icons-core")
-
-    // Essencial para corrigir KeyboardType.NumberDecimal
     implementation("androidx.compose.foundation:foundation")
-
-    // Navegação Compose (Essencial para o NavHost)
     implementation("androidx.navigation:navigation-compose:2.7.5")
-
-    // Tooling e Testes (Manter)
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
     testImplementation(libs.junit)
